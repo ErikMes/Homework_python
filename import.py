0fe764bb34011caa76f3a591805e3ab343a2d60a
ErikMes <mesropyanerik2003@mail.ru>
2022-09-08 15:11:04 +0400

    Homework

---
+++ import.py
@@ -0,0 +1,3 @@
+import lesson_dict
+print(lesson_dict.myfunc(4, 5))
+print(lesson_dict.myfunc1(5, 6))
--- lesson_dict.py
+++ lesson_dict.py
@@ -1,2 +1,18 @@
-x = 5
-print(x)
+# calculator
+def myfunc(a, b):
+    x = a + b
+    return x
+# print(myfunc(int(input('Enter the number--->')),int(input('Enter the number--->'))))
+def myfunc1(a, b):
+    x = a - b
+    return x
+# print(myfunc(int(input('Enter the number--->')),int(input('Enter the number--->'))))
+def myfunc2(a, b):
+    x = a * b
+    return x
+# print(myfunc(int(input('Enter the number--->')),int(input('Enter the number--->'))))
+def myfunc3(a, b):
+    x = a / b
+    return x
+# print(myfunc(int(input('Enter the number--->')),int(input('Enter the number--->'))))
+
